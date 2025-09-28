28059acfe0fef1131798e8a6495718c95ac0748f
vankass <akk4aga@gmail.com>
2025-09-28 12:15:09 +0300

    feat: add save tasks and work empty-message

--- script/script.js
+++ script/script.js
@@ -1,16 +1,35 @@
-const modal = document.querySelector('[data-js-modal]');
-const modalForm = document.querySelector('[data-js-modal-form]');
-const modalInput = document.querySelector('[data-js-modal-input]');
+const modal = document.querySelector("[data-js-modal]");
+const modalForm = document.querySelector("[data-js-modal-form]");
+const modalInput = document.querySelector("[data-js-modal-input]");
 
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-const template = document.querySelector('[data-js-todo-template]');
-const list = document.querySelector('[data-js-todo-list]');
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+const template = document.querySelector("[data-js-todo-template]");
+const list = document.querySelector("[data-js-todo-list]");
+const emptyMessage = document.querySelector("[data-js-todo-empty-message]");
 
-modalForm.addEventListener('submit', (e) => {
+renderTasks();
+
+modalForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const taskText = modalInput.value.trim();
   addTask(taskText);
 });
+
+function renderTasks() {
+  list.innerHTML = "";
+
+  if (tasks.length === 0) {
+    emptyMessage.style.display = "flex";
+  } else {
+    emptyMessage.style.display = "none";
+    tasks.forEach((task) => {
+      const node = template.content.firstElementChild.cloneNode(true);
+      node.dataset.taskId = task.id;
+      node.querySelector("[data-js-todo-item-span]").textContent = task.text;
+      list.appendChild(node);
+    });
+  }
+}
 
 function addTask(text) {
   if (!text) return;
@@ -18,17 +37,11 @@ function addTask(text) {
   const task = {
     id: Date.now().toString(),
     text: text,
-    done: false
-  }
+    done: false,
+  };
 
   tasks.push(task);
-
-  localStorage.setItem('tasks', JSON.stringify(tasks))
-
-  const node = template.content.firstElementChild.cloneNode(true);
-  node.dataset.taskId = task.id;
-  node.querySelector('[data-js-todo-item-span]').textContent = task.text;
-  list.appendChild(node);
-
-  modalInput.value = '';
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTasks();
+  modalInput.value = "";
 }
--- script/theme.js
+++ script/theme.js
@@ -1,10 +1,10 @@
 const html = document.documentElement;
-const themeButton = document.querySelector('[data-js-todo-theme-button]');
-const logo = document.querySelector('[data-js-todo-theme-img]');
-const empty = document.querySelector('[data-js-todo-empty-img]');
+const themeButton = document.querySelector("[data-js-todo-theme-button]");
+const logo = document.querySelector("[data-js-todo-theme-img]");
+const empty = document.querySelector("[data-js-todo-empty-img]");
 
-themeButton.addEventListener('click', () => {
-  const isDark = html.classList.toggle('dark');
-  logo.src = isDark ? 'img/sun.svg' : 'img/moon.svg';
-  empty.src = isDark ? 'img/detective-dark.svg' : 'img/detective-light.svg';
+themeButton.addEventListener("click", () => {
+  const isDark = html.classList.toggle("dark");
+  logo.src = isDark ? "img/sun.svg" : "img/moon.svg";
+  empty.src = isDark ? "img/detective-dark.svg" : "img/detective-light.svg";
 });
